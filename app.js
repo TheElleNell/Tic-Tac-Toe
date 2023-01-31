8034e80c7a8a2a1448ddf58a7fec3ec555e08230
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-01-30 20:38:53 -0500

    set-up

---
+++ app.js
@@ -0,0 +1,29 @@
+// Eleanor Brow Tic-Tac-Toe Game
+
+  // document.body.style.backgroundImage = "url(assets/water.gif)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// RE-LOAD PAGE CODE
+
+let resetButton = document.querySelector(".restart")
+
+resetButton.addEventListener("click", pageReLoad)
+
+function pageReLoad() {
+  location.reload()
+console.log(pageReLoad)
+
+}
+console.log(pageReLoad)
