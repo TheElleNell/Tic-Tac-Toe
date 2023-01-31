329c7146fc4cab2cecfec3f367580768376580b0
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-01-31 08:42:39 -0500

    updated html and css

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 // Eleanor Brow Tic-Tac-Toe Game
 
-  // document.body.style.backgroundImage = "url(assets/water.gif)"
+
+
+// WHALE FUNCTION 
+
+console.log("Before Whale Button")
+let whaleButton = document.querySelector(".playSpot1")
+
+whaleButton.addEventListener("click", whaleClick)
+
+function whaleClick() {
+  document.getElementById("playSpot1").src = "assets/whale-g69f6c2238_1280.png"
+
+
+}
+
+
+// TURTLE FUNCTION
 
 
 
 
 
+// CLICK EVENTS
 
 
 
-
-
-
-
-
-
-// RE-LOAD PAGE CODE
+// RE-LOAD PAGE WHEN GAME IS OVER
 
 let resetButton = document.querySelector(".restart")
 
