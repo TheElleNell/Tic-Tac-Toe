94128dae8a3099e2865fcb57139c774ca6773dcc
Steve JobsEleanor <eleanorabrow@gmail.com>
2023-01-31 16:32:34 -0500

    updated html and css

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 // Eleanor Brow Tic-Tac-Toe Game
 
 
-
 // CALLING BUTTONS AND UPDATING THEM
+
+// turning my buttons into an array 
 
 console.log("Before Buttons")
 
 let buttons = Array.from(document.getElementsByClassName("spot"))
 
-// turned my buttons into an array 
-
 console.log(buttons)
 
 
+// establishing players
 
-buttons.addEventListener("click", whaleClick)
+let player1 = "whale"
 
-function whaleClick() {
+let player2 = "turtle"
+
+
+// establishing the board
+
+let game = ["", "", "", "", "", "", "", "", ""]
+
+
+
+
+// buttons.addEventListener("click", whaleClick)
+
+function firstClick() {
+  let buttons = Array.from(document.getElementsByClassName("spot"))
+  let game = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  // loop through the board spots
+
+  buttons.forEach(button => {
+    button.addEventListener("click", (click) => {
+      let place = buttons.indexOf(click.target)
+      buttons[place].classList.add("player1")
+
+      console.log(buttons)
+      console.log
+    })
+  })
+  }
+  
+console.log(firstClick)
+
+
   document.getElementById("playSpot1").src = "assets/whale-g69f6c2238_1280.png"
 
 
-}
+
 
 
 // TURTLE FUNCTION
@@ -35,15 +66,15 @@ function whaleClick() {
 
 // RE-LOAD PAGE WHEN GAME IS OVER
 
-let restartButton = document.getElementsByClassName("restart")
+// let restartButton = document.getElementsByClassName("restart")
 
-console.log(restartButton)
+// console.log(restartButton)
 
-restartButton.addEventListener("click", pageReLoad)
+// restartButton.addEventListener("click", pageReLoad)
 
-function pageReLoad() {
-  location.reload()
-console.log(pageReLoad)
+// function pageReLoad() {
+//   location.reload()
+// console.log(pageReLoad)
 
-}
-console.log(pageReLoad)
+// }
+// console.log(pageReLoad)
